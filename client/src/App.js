10b2755da29f3f4a9187a30aeefd0bc0524dcf2f
HariKiran <harikiran.kante@gmail.com>
2023-12-09 18:06:07 +0530

    modified style of statistics and filters

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import Filter from '../src/Components/Filter/Filter';
-import Statistics from './Components/Statistics/Statistics';
-import './App.css';
+import React from "react";
+import Filter from "../src/Components/Filter/Filter";
+import Statistics from "./Components/Statistics/Statistics";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     <p className="heading">Articles Dashboard</p>
-      <div className='filter_section'>
-         <div className='filter_section_part'>
-            <Filter/>
-         </div>
+      <p className="heading">Articles Dashboard</p>
+      <div className="filter_section">
+        <div className="filter_section_part">
+          <Filter />
+        </div>
       </div>
-      <div className='Statistics_section'>
-          <Statistics/>
-         </div>
-
+      <div className="Statistics_section">
+        <Statistics />
+      </div>
     </div>
   );
 }
--- client/src/Components/Filter/Filter.jsx
+++ client/src/Components/Filter/Filter.jsx
@@ -7,27 +7,35 @@ import { RiArrowDropDownLine } from "react-icons/ri";
 
 function Filter() {
   const [isOpen, setIsOpen] = useState(false);
-  const [selectedValue,setSelectedValue] = useState('Impact')
-  const [selectedCategory,setSelectedCategory] = useState('Countries')
+  const [selectedValue, setSelectedValue] = useState("Impact");
+  const [selectedCategory, setSelectedCategory] = useState("Countries");
 
   return (
-    <div  className="Filter">
+    <div className="Filter">
       <div className="Filter_desc" onClick={() => setIsOpen(!isOpen)}>
         <h3>Filters</h3>
         <div className="filter_dropdown">
-           <p>Analyze by difference categories and values</p>
-           <div className="dropdown">
-             <RiArrowDropDownLine />
-           </div>
+          <p>Analyze by difference categories and values</p>
+          <div className="dropdown">
+            <RiArrowDropDownLine
+              className={`down-icon ${isOpen ? "active" : ""}`}
+            />
+          </div>
         </div>
       </div>
       {isOpen && (
         <div className="drop_content">
           <div className="Categories_section">
-            <Categories selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+            <Categories
+              selectedCategory={selectedCategory}
+              setSelectedCategory={setSelectedCategory}
+            />
           </div>
           <div className="Values_section">
-            <Values selectedValue={selectedValue} setSelectedValue={setSelectedValue} />
+            <Values
+              selectedValue={selectedValue}
+              setSelectedValue={setSelectedValue}
+            />
           </div>
         </div>
       )}
